8499ffbd3e068b9d7c727c8a652706c49b6e8e20
MOTMTOKEN <amirnilsson3@gmail.com>
2021-12-20 19:45:51 +0100

    Added display text with API

---
+++ script.js
@@ -0,0 +1,18 @@
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quotedisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+      .then(response => response.json())
+      .then(data => data.content)
+  }
+
+async function getNextQuote() {
+    const quote = await getRandomQuote()
+    quoteDisplayElement.innerText = quote
+    quoteInputElement.value = null
+}
+
+getNextQuote()
