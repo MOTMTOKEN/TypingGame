9464723f5bebc813362c037d141001dd350e033f
MOTMTOKEN <amirnilsson3@gmail.com>
2022-01-01 21:03:26 +0100

    finished game

--- script.js
+++ script.js
@@ -3,6 +3,36 @@ const quoteDisplayElement = document.getElementById('quotedisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
 
+quoteInputElement.addEventListener('input', () => {
+  
+  const arrQuote = quoteDisplayElement.querySelectorAll('span')
+  const arrValue = quoteInputElement.value.split('')
+
+  let correct = true
+
+  arrQuote.forEach((charachterSpan, index) => {
+
+    const charachter = arrValue[index]
+    if (charachter == null) {
+      charachterSpan.classList.remove('incorrect')
+      charachterSpan.classList.remove('correct')
+      correct = false
+    }
+    else if (charachter === charachterSpan.innerText){
+
+      charachterSpan.classList.add('correct')
+      charachterSpan.classList.remove('incorrect')
+      
+    } else {
+      charachterSpan.classList.add('incorrect')
+      charachterSpan.classList.remove('correct')
+      correct = false
+    }
+    
+  })
+  if (correct) { getNextQuote() }
+})
+
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
       .then(response => response.json())
@@ -11,9 +41,32 @@ function getRandomQuote() {
 
 async function getNextQuote() {
     const quote = await getRandomQuote()
-    quoteDisplayElement.innerText = quote
-    quoteInputElement.value = null
+    quoteDisplayElement.innerHTML = ''
 
+    quote.split('').forEach(charachter => {
+    const charachterSpan = document.createElement('span')
+    // charachterSpan.classList.add('correct')
+    charachterSpan.innerText = charachter 
+    quoteDisplayElement.appendChild(charachterSpan)
+    });
+    quoteInputElement.value = null
+    startTimer()
 }
 
 getNextQuote()
+
+
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
+
+
